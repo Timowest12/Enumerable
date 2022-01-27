090c3a334ba003669e3e3a3fb4ad0990daf78922
Oluwafemi <femikola23@gmail.com>
2022-01-27 21:02:47 +0400

    Add enumerable class

--- Enumerable.rb
+++ Enumerable.rb
@@ -1,13 +1,35 @@
 module MyEnumerable
+    def list
+        @list ||= []
+
     def all?
-     
+        return unless block_given?
+
+        value = true
+        list.each do |e|
+            value = false unless yeild e
+        end
+        value     
     end
   
     def any?
+        return unless block_given?
+
+        value = false
+        list.each do |e|
+            value = true if yeild e
+        end
+        value
       
     end
   
     def my_filter?
-     
+        return unless block_given?
+
+        arr = []
+        list.each do |e|
+            arr.push(e) if yeild e
+        end
+        arr
     end
   end
--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,16 @@
+require_relative 'MyEnumerable'
+
 class Mylist
+include MyEnumerable
     def initialize(*list)
         @list = list
     end
 end
+
+list = Mylist.new(1, 2, 3, 4)
+
+list.all? {|e| e < 5}
+list.all? {|e| e > 5}
+list.any? {|e| e == 2}
+list.any? {|e| e == 5}
+list.filter {|e| e.even?}
