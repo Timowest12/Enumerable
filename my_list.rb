320d45dd7d9729f204989b61689a873ce542984f
Timowest12 <timowester@gmail.com>
2022-01-27 18:32:47 +0100

    solve linter issues

--- my_list.rb
+++ my_list.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 require_relative 'Enumerable'
 
+# class MyList
 class Mylist
-include MyEnumerable
-    def initialize(*list)
-        @list = list
-    end
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
 end
 
 list = Mylist.new(1, 2, 3, 4)
 
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
-puts list.filter? {|e| e.even?}
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter?(&:even?)
